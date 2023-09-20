2ceb98e5e32626b682d59e9ccfa7d5def873154a
HebaMahdy <hebamahdy.86@gmail.com>
2023-09-20 12:58:28 +0300

    Using Whats App

--- lib/details.dart
+++ lib/details.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:whatsapp_share/whatsapp_share.dart';
+
+
+Future<void> share() async {
+  await WhatsappShare.share(
+    text: 'Example share text',
+    linkUrl: 'https://flutter.dev/',
+    phone: '911234567890',
+  );
+}
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
@@ -10,7 +20,7 @@ class DetailsScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Home Screen')),
       body: Center(
         child: ElevatedButton(
-          onPressed: () => context.go('/details'),
+          onPressed: () => share,
           child: const Text('Go to the Details screen'),
         ),
       ),
